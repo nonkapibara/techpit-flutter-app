70e11d90adf0f3d89a7ee8b1584e8773e3e95b23
nonchan <>
2021-02-13 21:42:28 +0900

    update

--- 07_Chapter/0708_source_code/lib/CommonList.dart
+++ 07_Chapter/0708_source_code/lib/CommonList.dart
@@ -28,41 +28,6 @@ class CommonList {
                           image: NetworkImage(
                               _data[index]['webformatURL'].toString())),
                     ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5.0, bottom: 40.0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                              child: Image.asset('images/likes.png',
-                                  width: 25.0, height: 25.0),
-                              padding: EdgeInsets.only(left: 20.0)),
-                          Container(
-                              child: Text(_data[index]['likes'].toString(),
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.amber[900])),
-                              padding: EdgeInsets.only(left: 10.0)),
-                          Container(
-                              child: Image.asset('images/favorites.png',
-                                  width: 20.0, height: 20.0),
-                              padding: EdgeInsets.only(left: 20.0)),
-                          Container(
-                              child: Text(_data[index]['favorites'].toString(),
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.amber[900])),
-                              padding: EdgeInsets.only(left: 10.0)),
-                        ],
-                      ),
-                    ),
-                    Divider(
-                      height: 0,
-                      thickness: 2,
-                      color: Colors.amber,
-                      indent: 1,
-                      endIndent: 1,
-                    ),
                   ],
                 ),
               );
